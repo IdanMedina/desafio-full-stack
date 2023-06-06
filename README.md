dcffc1d28c57f3b4b793a977112e30f94376b2b0
Idan <mquinhoz@hotmail.com>
2023-06-06 14:57:20 -0300

    feat: add .env.example del readme

--- README.md
+++
@@ -1,1 +0,0 @@
-# desafio-full-stack
--- back/src/main.ts
+++ back/src/main.ts
@@ -5,7 +5,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: 'http://localhost:3000'
+  })
   const config = new DocumentBuilder()
     .setTitle('Client contact register')
     .setDescription('Register your contacts!')
